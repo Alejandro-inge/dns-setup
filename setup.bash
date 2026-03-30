9f319190232ac87d12042214a02a96d72a478b9d
Alejandro-inge <galvanalejandro@protonmail.com>
2026-03-30 16:36:07 -0600

    Added setup.bash file

---
+++ setup.bash
@@ -0,0 +1,28 @@
+# Update the ubuntu repositories
+
+apt update
+
+# Open port 53 UDP/TCP
+
+ufw allow 53/tcp
+ufw allow 53/upd
+
+# Install bind9
+
+apt install bind9 -y
+
+sleep 1s
+
+# copy named and net.local.lan files to the /etc/bind directory
+
+cp ./named* net.local.lan /etc/bind/
+
+# Restart bind9 service
+
+systemctl restart bind9
+
+sleep 1s
+
+# Show bind9 status
+
+systemctl status bind9
